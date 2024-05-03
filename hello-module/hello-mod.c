2a508cd4e00bffd8227c5514c1f67d42d70925c1
William Wallace <66184236+williamwallace076@users.noreply.github.com>
2024-05-03 06:25:48 -0300

    Update hello-mod.c

--- hello-module/hello-mod.c
+++ hello-module/hello-mod.c
@@ -1,13 +1,13 @@
-/* Kernel Programming */
-#include <linux/module.h>  /* Needed by all modules */
-#include <linux/kernel.h>  /* Needed for KERN_ALERT */
-#include <linux/init.h>    /* Needed for init and exit macros */
+
+#include <linux/module.h>  
+#include <linux/kernel.h>  
+#include <linux/init.h>
 
 static int __init meu_inicio(void)
 {
    printk("<1>Hello world 1.\n");
    
-   // A non 0 return means init_module failed; module can't be loaded.
+   
    return 0;
 }
 
